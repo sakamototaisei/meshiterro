867f872ba098124813067a4fa95c59391660fddb
sakamototaisei <taisei0403089@gmail.com>
2021-05-14 14:55:02 +0000

    added favo

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6827917daa8f3fad8cd4776fb42a867b0f52278af489929eb3a952713a73c03ee6f2585e5b166a069a2cf59783891d9124bbdf16e3ad25d628d623111019825b'
